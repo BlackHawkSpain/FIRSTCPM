97d302679d269fd969ef3397e07ef68d5027cb1c
Antonio Soto <asotocid@gmail.com>
2022-11-10 10:17:20 +0100

    fondo con url

--- app/src/main/java/com/example/fundamentals/LoginActivity.java
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -3,27 +3,42 @@ package com.example.fundamentals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class LoginActivity extends AppCompatActivity {
 
    TextView enlaceSup;
-
+    ImageView car;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         enlaceSup = findViewById(R.id.enlaceSup);
+        //car= findViewById(R.id.car);
         enlaceSup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
             openSignUp();
             }
-        });
 
+        });
+        ImageView mGirl = findViewById(R.id.girl);
+
+        Glide.with(this).load("https://www.wsupercars.com/wallpapers-phone/Formula-1/Scuderia-Ferrari/2022-Formula1-Ferrari-F1-75-010-2400p.jpg")
+                //https://www.acfligue.org/wp-content/uploads/2021/12/custom_showroom_1654840098.png
+                .transition(DrawableTransitionOptions.withCrossFade(1000))
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+               .centerCrop()
+               //.circleCrop()
+                .into(mGirl);
 
     }
 
@@ -36,10 +51,9 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
 
 
-
-
-
-
-
     }
+
+
+
+
 }
